71869ba892a518144a5caf8c67cf3f13cbe5538d
Crowdbotics <team@crowdbotics.com>
2023-10-08 15:50:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "buyshopsendpay-43828",
+  url: "https://buyshopsendpay-43828.botics.co",
+  api: "https://buyshopsendpay-43828.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
